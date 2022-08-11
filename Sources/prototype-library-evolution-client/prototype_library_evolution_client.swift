e34ad66ae2f180e613c4403fed40c560e02a5f3c
Joakim Hassila <jocke@ordo.one>
2022-08-11 14:20:22 +0200

    Initial commit

---
+++ Sources/prototype-library-evolution-client/prototype_library_evolution_client.swift
@@ -0,0 +1,8 @@
+@main
+public struct prototype_library_evolution_client {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(prototype_library_evolution_client().text)
+    }
+}
---
+++ Tests/prototype-library-evolution-clientTests/prototype_library_evolution_clientTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import prototype_library_evolution_client
+
+final class prototype_library_evolution_clientTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(prototype_library_evolution_client().text, "Hello, World!")
+    }
+}
